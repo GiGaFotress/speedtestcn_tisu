a8b206ee313fe6367ea67a97eec0ce7d4938fea4
GiGaFotress <602330563@qq.com>
2020-06-13 05:56:04 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
   while true; do
   
-	wget -q -O-  "https://tisu-api.speedtest.cn/api/v2/speedup/query?source=www-index"|grep  '\"basic_down\":0'
+	wget -q -O-  "https://tisu-api.speedtest.cn/api/v2/speedup/query?source=www-index"|grep  '\"basic_down\":102400'
    echo "restart wan"
   sleep 15
 	
