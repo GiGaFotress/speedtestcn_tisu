98a26528cc126dae68c1104db801c1563da87e11
GiGaFotress <602330563@qq.com>
2020-06-15 10:41:14 +0000

    Update spre.sh

--- spre.sh
+++ spre.sh
@@ -1,8 +1,8 @@
- while true; do
-  wget -O- "https://tisu-api.speedtest.cn/api/v2/speedup/reopen?source=www"
-	wget -q -O-  "https://tisu-api.speedtest.cn/api/v2/speedup/query?source=www-index"|grep -v -q '\"basic_down\":0' && break
-	
-  stop_wan && restart_wan
-  sleep 15
+while true; do
+ wget -q -O- "https://tisu-api.speedtest.cn/api/v2/speedup/reopen?source=www"
+ wget -q -O-  "https://tisu-api.speedtest.cn/api/v2/speedup/query?source=www-index"|grep -v -q '\"basic_down\":0' && break
+ echo "retry"
+ stop_wan && restart_wan
+ sleep 15
 	
 done
